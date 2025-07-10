985bdd30b2de2846d2ae4a3bc110072da3a1f1fc
Jony2697 <ijony2697@gmail.com>
2025-07-10 12:54:35 +0600

    addedCart,update added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors=require('cors')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -26,19 +26,78 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const productsCollection=client.db('easyBuyDB').collection('products')
+    const productsCollection = client.db('easyBuyDB').collection('products');
+    const addToCartCollection = client.db('easyBuyDB').collection('addProducts');
 
     // products api
-    app.get('/products',async(req,res)=>{
-        const result=await productsCollection.find().toArray();
-        res.send(result)
+    app.get('/products', async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result)
     })
-    app.get('/products/:id',async(req,res)=>{
-        const id=req.params.id;
-        const query={_id:new ObjectId(id)};
-        const result=await productsCollection.findOne(query);
-        res.send(result)
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result)
     })
+
+    // Add to cart api
+
+    app.post('/addedProducts', async (req, res) => {
+      const { productId } = req.body;
+
+      const product = await productsCollection.findOne({ _id: new ObjectId(productId) });
+      if (!product) {
+        return res.status(404).json({ message: 'Product not found' });
+      }
+
+      const existingCartItem = await addToCartCollection.findOne({ productId: productId });
+
+      if (existingCartItem) {
+        const updated = await addToCartCollection.updateOne(
+          { productId: productId },
+          { $inc: { quantity: 1 } }
+        );
+        res.send({ message: 'Quantity increased', updated });
+      } else {
+
+        const cartItem = {
+          productId: productId,
+          title: product.title,
+          price: product.price,
+          image: product.image,
+          quantity: 1,
+        };
+        const result = await addToCartCollection.insertOne(cartItem);
+        res.send({ message: 'Product added to cart', result });
+      }
+    });
+
+    app.get('/addedProducts', async (req, res) => {
+      const result = await addToCartCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.delete('/addedProducts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addToCartCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.put('/addedProducts/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedCart = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          quantity: updatedCart.quantity
+        }
+      };
+      const result = await addToCartCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
